90ce19750741ba718bd220cb57b009f4ca51bc42
mdpCodes <mdpCodes@users.noreply.github.com>
2023-05-29 11:12:20 +0100

    wind and humidity elements
    html and js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind;
 }
 
 let apiKey = "bcfbb3o05cb4945t5f00bab1b879706f";
