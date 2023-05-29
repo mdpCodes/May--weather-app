78c1ab985a12621644c78e18e344068fa8009adf
mdpCodes <mdpCodes@users.noreply.github.com>
2023-05-29 10:33:02 +0100

    fixed SheCodes API URL
    used AthenaAI

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
 let apiKey = "bcfbb3o05cb4945t5f00bab1b879706f";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}`;
+let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
+  "New York"
+)}&key=${apiKey}&units=metric`;
 console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
