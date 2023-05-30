274b6fa269edf8f572e6440d97151f68b018b4ac
mdpCodes <mdpCodes@users.noreply.github.com>
2023-05-30 09:07:49 +0100

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return ` ${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -11,7 +34,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = response.data.time;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.innerHTML = response.data.icon;
 }
 let apiKey = "bcfbb3o05cb4945t5f00bab1b879706f";
