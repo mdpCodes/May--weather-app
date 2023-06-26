a7ba6cb18f03cf10d34774182df8069a2dcaba96
mdpCodes <mdpCodes@users.noreply.github.com>
2023-06-26 08:22:40 +0100

    corrected forecast code

--- src/app.js
+++ src/app.js
@@ -41,28 +41,27 @@ function displayForecast(response) {
         forecastHTML +
         `
       <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/${
-          response.data.condition.icon
-        }.png" alt="" width="42"/>
+        <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+        <img src="${forecastDay.condition.icon_url}" alt="" width="42"/>
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max">${Math.round(
-            forecastDay.temp.max
+            forecastDay.temperature.maximum
           )}</span>
           <span class="weather-forecast-temperature-min">${Math.round(
-            forecastDay.temp.min
+            forecastDay.temperature.minimum
           )}</span>
         </div>
       </div>`;
     }
   });
-  forecastHTML = `</div>`;
+  forecastHTML += `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "bcfbb3o05cb4945t5f00bab1b879706f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon={lon}&lat={lat}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
@@ -85,7 +84,7 @@ function displayTemperature(response) {
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
-  getForecast(response.data.coord);
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
